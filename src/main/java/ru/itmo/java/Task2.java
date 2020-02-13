4f4a084331a6ef48240b2e94fcf8f7dcb2dc5e4d
andrsuh <andrsuh13@yandex.ru>
2020-02-14 00:06:20 +0300

    add test

--- src/main/java/ru/itmo/java/Task2.java
+++ src/main/java/ru/itmo/java/Task2.java
@@ -13,4 +13,17 @@ public class Task2 {
         return 0.0; // put your code here
     }
 
+    /**
+     * Дан символ, верните его код. Не используйте явное приведение типов и любые библиотечные функции
+     */
+    int charCode(char c) {
+        return 0;
+    }
+
+    /**
+     * Даны 2 целых числа. Проверьте равны ли они
+     */
+    boolean testIfIntsEqual(Integer a, Integer b) {
+        return false;
+    }
 }
--- src/test/java/ru/itmo/java/Task2Test.java
+++ src/test/java/ru/itmo/java/Task2Test.java
@@ -3,6 +3,9 @@ package ru.itmo.java;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Random;
+
+@SuppressWarnings({"UnnecessaryBoxing", "deprecation", "CachedNumberConstructorCall"})
 public class Task2Test {
 
     @Test
@@ -13,4 +16,28 @@ public class Task2Test {
         Assert.assertEquals(0.9999, inst.getFractionalPart(17.9999), 1e-9);
     }
 
+    @Test
+    public void test_charCode() {
+        Task2 inst = new Task2();
+
+        Random random = new Random();
+
+        for (int i = 0; i < 100_000; i++) {
+            char code = (char) random.nextInt(Character.MAX_VALUE) ;
+            Assert.assertEquals(code, inst.charCode(code));
+        }
+    }
+
+    @Test
+    public void test_intsEqual() {
+        Task2 inst = new Task2();
+
+        Random random = new Random();
+
+        for (int i = 0; i < 100_000; i++) {
+            var a = random.nextInt(20);
+            var b = random.nextInt(20);
+            Assert.assertEquals(a == b, inst.testIfIntsEqual(new Integer(a), new Integer(b)));
+        }
+    }
 }
